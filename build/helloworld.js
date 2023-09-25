30eb992f7e9872ef5d5bed54233e35f9e5105fae
thelight0804 <thelight0804@gmail.com>
2023-09-25 15:57:37 +0900

    Add typescript files

---
+++ build/helloworld.js
@@ -0,0 +1,2 @@
+"use strict";
+let cal = (a, b) => a + b;
---
+++ src/helloworld.ts
@@ -0,0 +1,11 @@
+type yourName = string;
+type yourAge = number;
+type yourHeight = number;
+
+type idol = {
+	name: yourName;
+	age: yourAge;
+	height: yourHeight;
+}
+
+let member: idol = {name: "Yayoi", age: 14, height: 145};
